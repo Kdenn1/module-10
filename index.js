ad0808dfd336f304381657df83bed0cc9b7ee297
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-07-10 10:19:09 -0700

    added question prompts and base function to determine option outcomes

--- index.js
+++ index.js
@@ -9,9 +9,39 @@ const Manager = require("./Tests/Manager.test");
 //empty string 
 const employees = [];
 
-//function to start the app and prompt user with questions 
+//function to start the app and prompt user with questions and update the member list 
 function initApp() {
     startHtml(); //generate the html from question responses
     addMember(); //add a new member to the group 
 }
 
+//Prompts user with questions 
+function addMember() {
+    inquirer.prompt([{
+        //start passing messages into the terminal for user to answer 
+        message: "What is your team manager's name?",
+        name: "name"
+        },
+        {
+         message: "What is your employee ID?",
+         name: "ID"
+        },
+        {
+        message: "What is your email address?",
+        name: "email"
+        },
+        {
+        message: "What is your office number?",
+        name: "office"
+        },
+        {
+        type: "list",
+        message: "What position will you be adding to your team?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Finish building team"
+        ],
+        name: "role"
+    }])
+}
